1967b175c3168eca5bf75d212dd6578457c3e98c
Suhas-06 <119437679+Suhas-06@users.noreply.github.com>
2024-04-23 15:46:30 +0530

    Add files via upload

--- install-scripts.sh
+++ install-scripts.sh
@@ -1,10 +1,7 @@
-# mkdir -p /home/ayushsingh/jenkins
-# chmod 777 /home/ayushsingh/jenkins
 
 mkdir C:/Users/suhas/Desktop/jenkins
 icacls jenkins /grant Everyone:F
 docker build -t codercata5/jenkins .
-docker run -d -p 8080:8080 -v /home/ayushsingh/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock  codercata5/jenkins
 
 docker build -t jenkins-image .
-docker run -d -p 8080 -v C:/Users/suhas/Desktop/jenkins:/var/jenkins_home -v //./pipe/docker_engine://./pipe/docker_engine jenkins-image
+docker run -d -p 8080 -v C:/Users/suhas/Desktop/jenkins:/var/jenkins_home -v //./pipe/docker_engine:/var/run/docker.sock jenkins-image
