b1ac893b1e2c48aebb99651d87419c1ef59e1d0b
Suhas-06 <119437679+Suhas-06@users.noreply.github.com>
2024-04-26 01:04:44 +0530

    Delete install-scripts.sh

--- install-scripts.sh
+++
@@ -1,7 +0,0 @@
-
-mkdir C:/Users/suhas/Desktop/jenkins
-icacls jenkins /grant Everyone:F
-docker build -t codercata5/jenkins .
-
-docker build -t jenkins-image .
-docker run -d -p 8080 -v C:/Users/suhas/Desktop/jenkins:/var/jenkins_home -v //./pipe/docker_engine:/var/run/docker.sock jenkins-image
